adb49c7ffee68e553448b0cd4755a0ff34ba5011
IrishBruse <Econn50@outlook.com>
2024-06-27 12:11:26 +0100

    Update zsh

--- zsh/commands.zsh
+++ zsh/commands.zsh
@@ -1,10 +1,16 @@
 alias cd="z"
 alias lz="lazygit"
 alias ls="eza -a"
-alias zshrc="code ~/dotfiles/zsh/zshrc.sh"
+alias ll="eza -al"
 alias watch="watch exec"
-alias reload="exec zsh"
+alias reload="echo 'Reloading enviroment variables';exec zsh"
+alias path="echo $PATH | tr ':' '\n'"
+alias ..="cd .."
+alias ~="cd ~"
+alias econneely="gh auth switch --user econneely"
+alias irishbruse="gh auth switch --user IrishBruse"
 
+alias zshrc="code ~/dotfiles/zsh/zshrc.sh"
 alias dot="code ~/dotfiles/"
 
 c() {
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -2,8 +2,6 @@ eval "$(oh-my-posh init zsh --config ~/dotfiles/zsh/oh-my-posh.yaml)"
 eval "$(zoxide init zsh)"
 eval "$(fzf --zsh)"
 
-export PATH=$PATH:~/go/bin
-
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
@@ -17,7 +15,6 @@ zinit light zpm-zsh/clipboard # pipe to clip
 zinit light zsh-users/zsh-completions # ghost text completion
 zinit light zsh-users/zsh-autosuggestions
 zinit snippet 'https://github.com/robbyrussell/oh-my-zsh/raw/master/plugins/git/git.plugin.zsh'
-zinit snippet ~/gh.sh
 
 source ~/dotfiles/zsh/commands.zsh
 source ~/gp.sh
