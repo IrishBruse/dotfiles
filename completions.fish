202491423ef721a3353f1fdf5f461793ec664d23
Ethan Conneely <econn50@outlook.com>
2024-08-06 02:26:06 +0100

    Completions

---
+++ completions.fish
@@ -0,0 +1,5 @@
+# Fish completions
+wezterm shell-completion --shell fish >.config/fish/completions/wezterm.fish
+curl https://raw.githubusercontent.com/Orange-OpenSource/hurl/master/completions/hurl.fish >.config/fish/completions/hurl.fish
+curl https://raw.githubusercontent.com/Orange-OpenSource/hurl/master/completions/hurlfmt.fish >.config/fish/completions/hurlfmt.fish
+wezterm shell-completion --shell fish >.config/fish/completions/wezterm.fish
