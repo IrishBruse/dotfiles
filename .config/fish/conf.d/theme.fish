b9db6d3d1a4f0be1bdc21a93566f5921638993df
Ethan Conneely <econneely@g-p.com>
2026-01-05 12:25:04 +0000

    Update

---
+++ .config/fish/conf.d/theme.fish
@@ -0,0 +1,38 @@
+set -g fish_color_autosuggestion brblack
+set -g fish_color_cancel 00ff00
+set -g fish_color_command magenta
+set -g fish_color_comment 5C6370
+set -g fish_color_cwd 00ff00
+set -g fish_color_cwd_root 00ff00
+set -g fish_color_end yellow
+set -g fish_color_error red
+set -g fish_color_escape 56b6c2
+set -g fish_color_history_current 56b6c2
+set -g fish_color_host 00ff00
+set -g fish_color_host_remote 00ff00
+set -g fish_color_keyword magenta
+set -g fish_color_match ff00ff
+set -g fish_color_normal normal
+set -g fish_color_operator yellow
+set -g fish_color_option blue
+set -g fish_color_param bcbcbc
+set -g fish_color_quote green
+set -g fish_color_redirection yellow
+set -g fish_color_search_match brred
+set -g fish_color_selection ff00ff
+set -g fish_color_status 00ff00
+set -g fish_color_user 00ff00
+set -g fish_color_valid_path cyan
+set -g fish_pager_color_background
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description yellow --italics
+set -g fish_pager_color_prefix normal --bold --underline=single
+set -g fish_pager_color_progress brwhite --bold --background=cyan
+set -g fish_pager_color_secondary_background
+set -g fish_pager_color_secondary_completion
+set -g fish_pager_color_secondary_description
+set -g fish_pager_color_secondary_prefix
+set -g fish_pager_color_selected_background --reverse
+set -g fish_pager_color_selected_completion
+set -g fish_pager_color_selected_description
+set -g fish_pager_color_selected_prefix
