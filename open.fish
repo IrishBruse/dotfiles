e48418db3d24feef65322e778ee117d608603945
Ethan <econn50@outlook.com>
2026-04-19 04:11:38 +0100

    Fix

--- open.fish
+++ open.fish
@@ -1,8 +1,5 @@
 #!/usr/bin/env fish
 
-zoxide init fish --cmd cd >.config/fish/conf.d/zoxide.fish
-fzf --fish >.config/fish/conf.d/fzf.fish
-
 if test (uname) = Linux
     dconf read /com/linuxmint/install/installed-apps | string replace -a \' \" | jq >misc/apt.json
     dconf dump / >.config/dconf/user.ini
@@ -12,6 +9,9 @@ if test (uname) = Darwin
     brew bundle dump --no-vscode -f --file=misc/Brewfile
     /opt/homebrew/bin/brew shellenv >.config/fish/conf.d/brew.fish
 end
+
+zoxide init fish --cmd cd >.config/fish/conf.d/zoxide.fish
+fzf --fish >.config/fish/conf.d/fzf.fish
 
 # Ignore new changes to files
 # undo with
