d7a33337f794898f71da7ce8cb7889d301c87863
Ethan Conneely <econneely@g-p.com>
2026-04-22 16:12:48 +0100

    Link cursor

--- init.fish
+++ init.fish
@@ -5,5 +5,8 @@ acli completion fish >.config/fish/completions/acli.fish
 rm .cursor/ 2>/dev/null
 mkdir .cursor/
 
+# ln -s <real source> <symlinked target>
+
 ln -s ~/.cursor/mcp.json ~/dotfiles/.cursor 2>/dev/null || echo Skipped mcp.json
 ln -s ~/.cursor/cli-config.json ~/dotfiles/.cursor 2>/dev/null || echo Skipped cli-config.json
+ln -s ~/dotfiles/.agents/skills/ ~/.cursor/skills 2>/dev/null || echo Skipped .agents/skills/
