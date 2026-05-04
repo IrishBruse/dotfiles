919e6ad4246627e1df9107507f2e3e72ede1a666
Ethan <econn50@outlook.com>
2026-05-04 23:42:17 +0100

    model

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -27,7 +27,7 @@ alias ll="eza -la --no-permissions --group-directories-first"
 alias reload="clear;exec fish"
 
 alias ac="agent --continue"
-alias a="agent"
+alias ao="agent --model claude-4.6-opus-high-thinking"
 alias ap="agent --mode=plan agent"
 alias aa="agent --mode=ask agent"
 
