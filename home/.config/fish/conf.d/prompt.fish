0cb033402b9d0346038992b1082ad460e2ef9095
Ethan <econn50@outlook.com>
2026-05-16 17:07:25 +0100

    Add time

--- home/.config/fish/conf.d/prompt.fish
+++ home/.config/fish/conf.d/prompt.fish
@@ -2,7 +2,18 @@ function fish_prompt
     set -l exit_code $status
 
     if not set -e transient_prompt
-        printf (set_color cyan)(prompt_pwd --full-length-dirs=20)(set_color normal)
+        printf (set_color cyan)(prompt_pwd --full-length-dirs=20)
+
+        set -l ms $CMD_DURATION
+        set -l dur_str
+        if test $ms -lt 1000
+            set dur_str "$ms"ms
+        else if test $ms -lt 10000
+            set dur_str (math -s1 "$ms / 1000")s
+        else
+            set dur_str (math -s0 "$ms / 1000")s
+        end
+        printf (set_color brblack)" %s"(set_color normal) $dur_str
 
         if test $exit_code -ne 0
             printf (set_color red)" $exit_code "(set_color normal)
