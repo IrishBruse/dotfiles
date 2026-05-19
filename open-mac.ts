ddce6f904ac416b17953453370953643ae2ebd99
Ethan Conneely <econneely@g-p.com>
2026-05-19 09:27:05 +0100

    update

--- open-mac.ts
+++ open-mac.ts
@@ -1,13 +1,12 @@
 import { mkdirSync } from "node:fs";
 import { join } from "node:path";
 import { mergeCursorCliPermissions, repoDir } from "./open-shared.ts";
-import { $, q } from "./shell.ts";
+import { $ } from "./shell.ts";
 
 const repo = repoDir(import.meta.url);
-const brewFish = join(repo, ".config/fish/conf.d/brew.fish");
 mkdirSync(join(repo, ".config/fish/conf.d"), { recursive: true });
 
 $(`brew bundle dump --no-vscode -f --file=./brewfile`);
-$(`brew shellenv >${q(brewFish)}`);
+$(`brew shellenv >home/.config/fish/conf.d/brew.fish`);
 
 mergeCursorCliPermissions(repo);
