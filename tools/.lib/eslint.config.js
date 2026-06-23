156064dc77119c501ff17b610b07a530d89392fc
Ethan <econn50@outlook.com>
2026-06-23 22:49:37 +0100

    tools(.lib): update eslint.config

--- tools/.lib/eslint.config.js
+++ tools/.lib/eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["node_modules/**", ".bin/**", ".lib/**"]
+    ignores: ["node_modules/**", ".bin/**", ".lib/**", "homepage/**"]
   },
   {
     files: ["**/*.ts"],
