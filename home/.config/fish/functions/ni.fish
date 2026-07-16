b77f7aa057a277341f38dec5ab966fd207cb4d13
Ethan Conneely <econneely@g-p.com>
2026-07-16 13:17:28 +0100

    Simplify

--- home/.config/fish/functions/ni.fish
+++ home/.config/fish/functions/ni.fish
@@ -1,16 +1,8 @@
 function ni --wraps "npm install"
-    set -l log (mktemp)
-    npm install --save $argv 2>&1 | tee $log
-    set -l exit_code $pipestatus[1]
-    if test $exit_code -ne 0
-        if string match -q '*403*' -- (cat $log)
-            rm -f $log
-            token
-            or return $status
-            npm install --save $argv
-            return $status
-        end
+    npm install --save $argv
+    if test $status -ne 0
+        token || return $status
+        npm install --save $argv
+        return $status
     end
-    rm -f $log
-    return $exit_code
 end
