5acf3fcfd6ccf2709dcf4ad7af1ad6d4def0bc7c
Ethan Conneely <econneely@g-p.com>
2026-08-13 17:33:44 +0100

    macos: open.ts

--- macos/open.ts
+++ macos/open.ts
@@ -5,7 +5,6 @@ import { homedir } from "node:os";
 import { join } from "node:path";
 
 import { $, q } from "../shell.ts";
-import { syncJiraSkill } from "../shared/jira-skill.ts";
 import { syncBrew } from "./shared/brew.ts";
 import { repoDir, repoRoot } from "./shared/repo.ts";
 
@@ -25,5 +24,4 @@ const platform = repoDir(import.meta.url);
 const repo = repoRoot(import.meta.url);
 
 syncBrew(platform, repo);
-syncJiraSkill(repo);
 reloadJiraSyncLaunchAgent();
